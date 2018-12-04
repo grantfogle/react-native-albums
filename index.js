28964f628e0463efc6dc39ebbe855cb04b0887f2
grantfogle <grantjfogle@gmail.com>
2018-12-04 10:50:44 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/** @format */
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+const App = () => (
+    <Header headerText={'Albums'} />
+);
+
+AppRegistry.registerComponent('albums2', () => App);
